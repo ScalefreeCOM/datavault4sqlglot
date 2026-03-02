dd40c8bf56ab8440a85beb09de1027e2d9883fe5
mszerencse <mszerencse@scalefree.com>
2026-03-02 18:33:07 +0100

    sourcemodel to path

--- datavault4sqlglot/__init__.py
+++ datavault4sqlglot/__init__.py
@@ -2,7 +2,15 @@ from .generators.stage import StageGenerator
 from .generators.hub import HubGenerator
 from .generators.link import LinkGenerator
 from .generators.satellite import SatelliteGenerator
+from .metadata.source import SourceModel
 from .config import config
 
-__all__ = ["StageGenerator", "HubGenerator", "LinkGenerator", "SatelliteGenerator", "config"]
+__all__ = [
+    "StageGenerator",
+    "HubGenerator",
+    "LinkGenerator",
+    "SatelliteGenerator",
+    "SourceModel",
+    "config",
+]
 
