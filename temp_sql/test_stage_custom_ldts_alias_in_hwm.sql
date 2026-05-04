3a8833f3dad1e51094811b73b671cc2a75fd81a3
Ulf Mattern <Umattern@scalefree.com>
2026-05-04 15:43:16 +0200

    .

--- temp_sql/test_stage_custom_ldts_alias_in_hwm.sql
+++
@@ -1,3 +0,0 @@
--- STAGE -- custom ldts_alias=load_ts in incremental HWM filter
-
-SELECT *, NULLIF(LOWER(MD5(NULLIF(CAST(REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(UPPER(CONCAT(COALESCE(CONCAT('\"', REPLACE(REPLACE(REPLACE(TRIM(CAST("order_id" AS VARCHAR(4000))), '\\', '\\\\'), '"', '\"'), '^^', '--'), '\"'), '^^'))), CHR(9), ''), CHR(10), ''), CHR(11), ''), CHR(13), '') AS VARCHAR(4000)), '^^'))), '00000000000000000000000000000000') AS "hk_order" FROM "raw.orders" WHERE load_ts > (SELECT MAX(load_ts) FROM "stage_view" WHERE load_ts <> '9999-12-31')
--- temp_sql/test_stage_newline_removal_regexp.sql
+++
@@ -1,3 +0,0 @@
--- STAGE -- REGEXP_REPLACE — whitespace normalization before hashing
-
-SELECT *, NULLIF(LOWER(MD5(NULLIF(CAST(REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(UPPER(CONCAT(COALESCE(CONCAT('\"', REPLACE(REPLACE(REPLACE(TRIM(CAST("order_id" AS VARCHAR(4000))), '\\', '\\\\'), '"', '\"'), '^^', '--'), '\"'), '^^'))), CHR(9), ''), CHR(10), ''), CHR(11), ''), CHR(13), '') AS VARCHAR(4000)), '^^'))), '00000000000000000000000000000000') AS "hk_order" FROM "raw.orders"
